ff41869abd88e3af478e180a0384bcf27b867c90
Guilherme <guilheeerme922@gmail.com>
2023-10-22 15:12:02 -0300

    Adiciona o clarity

--- src/client/initial-app/index.tsx
+++ src/client/initial-app/index.tsx
@@ -44,3 +44,17 @@ main();
     document.head.appendChild(script);
   });
 }
+
+// Clarity
+{
+  addEventListener('load', () => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "jeo5332tot");`;
+    document.head.appendChild(script);
+  });
+}
